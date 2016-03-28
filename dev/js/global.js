34777cf978dacfba3bb60872f78a6238c560706d
Luke Harrison <luke@lukeharrison.net>
2016-03-28 23:35:02 +0100

    Started work on touch events

--- dev/js/global.js
+++ dev/js/global.js
@@ -4,8 +4,6 @@
 |--------------------------------------------------------------------
 */
 
-
-
 (function(){
 	var banner = document.querySelector(".banner--about");
 	var full = document.querySelector(".feature-full");
@@ -14,19 +12,37 @@
 	var x = window.innerWidth / 2;
 	var oldX;
 
-	var bannerOver = function(){
-		full.classList.remove("inactive");
-		banner.onmousemove = function(e){
-			x = window.innerWidth - e.pageX;
+	if(!Modernizr.touchevents){ 
+
+		banner.addEventListener("mouseover",  function(){
+			full.classList.remove("inactive");
+			banner.onmousemove = function(e){
+				x = window.innerWidth - e.pageX;
+				full.style.setProperty("-webkit-clip-path", `inset(0 0 0 ${x}px)`);
+			};
+		});
+
+		banner.addEventListener("mouseout", function(){
+			full.classList.add("inactive");
+			full.style.removeProperty("-webkit-clip-path");
+		});
+
+	}
+	else if(Modernizr.touchevents) {
+
+		banner.addEventListener("touchmove", function(e){
+			e.preventDefault();
+			full.classList.remove("inactive");
+			x = e.touches[0].clientX;
 			full.style.setProperty("-webkit-clip-path", `inset(0 0 0 ${x}px)`);
-		};
-	};
+			full.style.setProperty("clip-path", `inset(0 0 0 ${x}px)`);
+		});
 
-	banner.addEventListener("mouseover", bannerOver);
-
-	banner.addEventListener("mouseout", function(){
-		full.classList.add("inactive");
-		full.style.removeProperty("-webkit-clip-path");
-	});
+		banner.addEventListener("touchend", function(e){
+			full.classList.add("inactive");
+			document.querySelector(".feature-full").removeAttribute("style");
+			//document.querySelector("h1").style.setProperty("display", "none");
+		});
+	}
 
 })();
