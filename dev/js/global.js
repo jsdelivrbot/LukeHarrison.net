d5a8d738217abf6ff5380a3bb5867b894fee9f8a
Luke Harrison <luke@lukeharrison.net>
2016-04-05 23:23:23 +0100

    changes

--- dev/js/global.js
+++ dev/js/global.js
@@ -13,24 +13,34 @@
 	// http://stackoverflow.com/questions/27558996/how-can-i-test-for-clip-path-support
 	var areClipPathShapesSupported = function () {var base = 'clipPath', prefixes = [ 'webkit', 'moz', 'ms', 'o' ], properties = [ base ], testElement = document.createElement( 'testelement' ), attribute = 'polygon(50% 0%, 0% 100%, 100% 100%)'; for ( var i = 0, l = prefixes.length; i < l; i++ ) {var prefixedProperty = prefixes[i] + base.charAt( 0 ).toUpperCase() + base.slice( 1 ); properties.push( prefixedProperty ); } for ( var i = 0, l = properties.length; i < l; i++ ) {var property = properties[i]; if ( testElement.style[property] === '' ) {testElement.style[property] = attribute; if ( testElement.style[property] !== '' ) {return true; } } } return false; },
 
-		// Set variables, cas
+	// Set variables
 		banner = document.querySelector(".banner--about"),
 		full = document.querySelector(".feature-full"),
 		lines = document.querySelector(".feature-lines"),
+		fullText = document.querySelector(".feature-text--developer"),
+		linesText = document.querySelector(".feature-text--designer"),
 		defaultWidth,
 		defaultHeight,
 		pos = window.innerWidth / 2,
+		opacityLines,
+		opacityFull,
+		moveArea,
 		clipPath = areClipPathShapesSupported(),
 
 	// Create function to set default feature state
 		resetBanner = function(){
+			// Reset text opacity
+			fullText.removeAttribute("style");
+			linesText.removeAttribute("style");		
+
+			// Reset vertical line
 			if(clipPath){
-				full.style.setProperty("-webkit-clip-path", "inset(0 0 0 50%)"); 
+				full.style.setProperty("-webkit-clip-path", "inset(0 0 0 50%)");
 			}
 			else {
 				defaultWidth = window.innerWidth;
-				defaultHeight = (window.innerHeight + 100);
-				full.style.setProperty("clip", `rect(0px, ${defaultWidth}px, ${defaultHeight}px, ${defaultWidth*0.4952}px)`);
+				defaultHeight = (window.innerHeight + 300);
+				full.style.clip = `rect(0px, ${defaultWidth}px, ${defaultHeight}px, ${defaultWidth*0.4952}px)`;
 			}
 		};
 
@@ -49,21 +59,44 @@
 
 		// Define function which fires when mouse enters feature
 		var bannerEnter = function(){
-			full.classList.remove("inactive");
-				banner.onmousemove = function(e){
-					pos = window.innerWidth - e.pageX;
-					if(clipPath){
-						full.style.setProperty("-webkit-clip-path", `inset(0 0 0 ${pos}px)`); 
-					}
-					else {
-						full.style.setProperty("clip", `rect(0px, ${defaultWidth}px, ${defaultHeight}px, ${pos}px)`);
-					}
-				};
+			banner.classList.remove("inactive");
+
+			// Get width of body
+			moveArea = document.body.clientWidth;
+
+			// Define what happens as mouse moves
+			banner.onmousemove = function(e){
+				pos = moveArea - e.pageX;
+
+				// calculate percentage of pos relative to window width
+				if(pos >= (moveArea / 2)){
+					// Right half
+					opacityLines = e.pageX / (moveArea / 2);
+					opacityFull = 1;
+				}
+				else {
+					// Left half
+					opacityFull = 2 - (e.pageX / (moveArea / 2));
+					opacityLines = 1;
+				}
+
+				// change text opacity
+				fullText.style.opacity = opacityLines;
+				linesText.style.opacity = opacityFull;
+
+				// Move vertical line
+				if(clipPath){
+					full.style.setProperty("-webkit-clip-path", `inset(0 0 0 ${pos}px)`); 
+				}
+				else {
+					full.style.clip = `rect(0px, ${defaultWidth}px, ${defaultHeight}px, ${pos}px)`;
+				}
+			};
 		},
 
 		// Define function which fires when mouse leaves feature
 			bannerLeave = function(){
-				full.classList.add("inactive");
+				banner.classList.add("inactive");
 				resetBanner();
 		};
 
