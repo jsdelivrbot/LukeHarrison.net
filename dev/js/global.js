28509ee19a653bca9115b1b2fca7c3eb0df4d51f
Luke Harrison <luke.harrison@plus.net>
2016-04-05 14:03:48 +0100

    Commented on JS

--- dev/js/global.js
+++ dev/js/global.js
@@ -4,9 +4,10 @@
 |--------------------------------------------------------------------
 */
 
-"use strict";
+// Pass in window and document object to increase lookup speed
+(function(window, document){
 
-(function(){
+	"use strict";
 
 	// Check if clip-paths are supported function
 	// http://stackoverflow.com/questions/27558996/how-can-i-test-for-clip-path-support
@@ -21,6 +22,7 @@
 		pos = window.innerWidth / 2,
 		clipPath = areClipPathShapesSupported(),
 
+	// Create function to set default feature state
 		resetBanner = function(){
 			if(clipPath){
 				full.style.setProperty("-webkit-clip-path", "inset(0 0 0 50%)"); 
@@ -32,18 +34,20 @@
 			}
 		};
 
-	// Set to default
+	// Set to default feature state
 	resetBanner(); 
 
+	// If no touch events and if higher then the med breakpoint then enable interaction
 	if(!Modernizr.touchevents && bp.min("med")){
 
-		// Reposition on window resize
+		// Reposition on window resize if we're forced to use clip instead of clip-path
 		if(!clipPath){
 			window.onresize = function(){
 				resetBanner(); 
 			}
 		}
 
+		// Define function which fires when mouse enters feature
 		var bannerEnter = function(){
 			full.classList.remove("inactive");
 				banner.onmousemove = function(e){
@@ -57,11 +61,13 @@
 				};
 		},
 
-		bannerLeave = function(){
-			full.classList.add("inactive");
-			resetBanner();
+		// Define function which fires when mouse leaves feature
+			bannerLeave = function(){
+				full.classList.add("inactive");
+				resetBanner();
 		};
 
+		// Attach event liseners and if not availble use legacy attachEvent
 		if(banner.addEventListener){
 			banner.addEventListener("mouseenter", bannerEnter);
 			banner.addEventListener("mouseleave", bannerLeave);
@@ -71,7 +77,6 @@
 			banner.attachEvent("mouseleave", bannerLeave);
 		}
 
-
 	}
 
-})();
+})(window, document);
