602dfb65891a5e16db8f2e81785774f4da1972b4
Luke Harrison <luke@lukeharrison.net>
2016-04-18 17:41:32 +0100

    Portfolio work

---
+++ dev/js/partials/modules/slider.js
@@ -0,0 +1,56 @@
+module.exports = (function(document){
+
+	var slider = document.querySelectorAll(".portfolio-item__screens"),
+		length = slider.length,
+		i,
+		sliderControl,
+		target,
+		active,
+		next,
+		previous,
+		parent;
+
+	sliderControl = function(e){
+		target = e.target || e.srcElement;
+		parent = e.path[2];
+		active = parent.querySelector(".portfolio-item__slide.active");
+
+		if(target.classList.contains("portfolio-items__controls-prev")){
+			next = active.previousElementSibling;
+
+		}
+		else if(target.classList.contains("portfolio-items__controls-next")){
+			next = active.nextElementSibling; 
+		}
+
+		if(next){
+			if(active.classList.contains("active")){
+				active.classList.remove("active"); 
+
+				if(target.classList.contains("portfolio-items__controls-next")){
+					active.classList.add("prev");
+				}
+			}
+			if(!next.classList.contains("active")) {
+				next.classList.add("active");
+				next.classList.remove("prev");
+			}
+		}
+	}
+
+	for(i = 0; i < length; i++) {
+		if(slider[i].addEventListener){
+			slider[i].addEventListener("click", function(e){
+				sliderControl(e);
+			});
+		}
+		else {
+			slider[i].attachEvent("onclick", function(e){
+				sliderControl(e); 
+			}); 
+		}
+	}
+
+
+
+})(document);
--- dev/js/portfolio.js
+++ dev/js/portfolio.js
@@ -1,1 +1,3 @@
 var global = require("./partials/modules/global.js");
+var slider = require("./partials/modules/slider.js");
+
