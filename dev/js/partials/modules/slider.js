1cfafb8d943d90450c2cda62a9adcfe9b97dca96
Luke Harrison <luke@lukeharrison.net>
2016-04-21 00:55:12 +0100

    Need to fix swipe bug

--- dev/js/partials/modules/slider.js
+++ dev/js/partials/modules/slider.js
@@ -1,7 +1,9 @@
 module.exports = (function(document, window){
 
 	var classList = require("../polyfills/classlist.js");
-	var Modernizr = require("../vendor/modernizr-custom.js");
+	var Modernizr = require("../vendor/modernizr-custom.js"); 
+	var getClosest = require("../vendor/getClosest.js");
+	var swipeDetect = require("../vendor/swipeDetect.js");
 
 	var slider = document.querySelectorAll(".portfolio-item__screens.multiple"),
 		length = slider.length,
@@ -16,95 +18,9 @@ module.exports = (function(document, window){
 		next,
 		previous,
 		parent,
-		eventObj,
-		getClosest,
-		swipedetect,
 		screenContainer,
 		slides,
 		activePos;
-
-	// jquery Closest() vanilla JS function
-	// http://gomakethings.com/climbing-up-and-down-the-dom-tree-with-vanilla-javascript/
-	getClosest = function (elem, selector) {
-	    var firstChar = selector.charAt(0);
-	    // Get closest match
-	    for ( ; elem && elem !== document; elem = elem.parentNode ) {
-	        // If selector is a class
-	        if ( firstChar === '.' ) {
-	            if ( elem.classList.contains( selector.substr(1) ) ) {
-	                return elem;
-	            }
-	        }
-	        // If selector is an ID
-	        if ( firstChar === '#' ) {
-	            if ( elem.id === selector.substr(1) ) {
-	                return elem;
-	            }
-	        } 
-	        // If selector is a data attribute
-	        if ( firstChar === '[' ) {
-	            if ( elem.hasAttribute( selector.substr(1, selector.length - 2) ) ) {
-	                return elem;
-	            }
-	        }
-	        // If selector is a tag
-	        if ( elem.tagName.toLowerCase() === selector ) {
-	            return elem;
-	        }
-	    }
-	    return false;
-	};
-
-	// Swipe detection function
-	// http://www.javascriptkit.com/javatutors/touchevents2.shtml
-	swipedetect = function(el, callback){
-	  
-	    var touchsurface = el,
-	    swipedir,
-	    startX,
-	    startY,
-	    dist,
-	    distX,
-	    distY,
-	    threshold = 100, //required min distance traveled to be considered swipe
-	    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-	    allowedTime = 300, // maximum time allowed to travel that distance
-	    elapsedTime,
-	    startTime,
-	    handleswipe = callback || function(swipedir){}
-	  
-	    touchsurface.addEventListener('touchstart', function(e){
-	        var touchobj = e.changedTouches[0]
-	        swipedir = 'none'
-	        dist = 0
-	        startX = touchobj.pageX
-	        startY = touchobj.pageY
-	        startTime = new Date().getTime() // record time when finger first makes contact with surface
-	        e.preventDefault()
-              eventObj = e;
-	    }, false)
-	  
-	    touchsurface.addEventListener('touchmove', function(e){
-	        //e.preventDefault() // prevent scrolling when inside DIV
-	    }, false)
-	  
-	    touchsurface.addEventListener('touchend', function(e){
-	        var touchobj = e.changedTouches[0]
-	        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-	        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-	        elapsedTime = new Date().getTime() - startTime // get time elapsed
-	        if (elapsedTime <= allowedTime){ // first condition for awipe met
-	            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-	                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-	            }
-	            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-	                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-	            }
-	        }
-	        handleswipe(swipedir)
-	        e.preventDefault()
-	    }, false)
-	}
 
 	sliderClick = function(e){
 		target = e.target || e.srcElement;
@@ -189,16 +105,19 @@ module.exports = (function(document, window){
 			}
 
 			// Add touch events
+
 			if(Modernizr.touchevents){
-				swipedetect(slider[i], function(swipedir){
+				swipeDetect(slider[i], function(swipedir){
 					
 					parent = getClosest(eventObj.target, '.portfolio-item__screens');
 
 					if (swipedir == 'right') {
+						console.log("right")
 						sliderMove("previous", parent);
 					}
 					if (swipedir == 'left') {
-						sliderMove("next", parent);
+						console.log("left")
+						sliderMove("next", parent); 
 					}
 				})
 			}
@@ -217,7 +136,6 @@ module.exports = (function(document, window){
 			screenContainer.style.left = 0 + "%"; 
 
 		}
-
 
 
 	})(slider);
