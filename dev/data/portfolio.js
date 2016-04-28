f4ee197bdf0e056b406781f3ee3189e5adca0cc4
Luke Harrison <luke.harrison@plus.net>
2016-04-28 13:54:17 +0100

    Portfolio work

--- dev/data/portfolio.js
+++ dev/data/portfolio.js
@@ -10,8 +10,38 @@ var portfolio = {
 			}
 		],
 		"description": [
-			"I have key skills which stretch across the project life cycle; from designing UX driven wireframes and mockups through to developing high quality code focusing on performance. I have key skills which stretch across the project life cycle; from designing UX driven wireframes and mockups through to developing high quality code focusing on performance.",
-			"I have key skills which stretch across the project life cycle; from designing UX driven wireframes and mockups through to developing high quality code focusing on performance."
+			"Orion is a mobile-first HTML/CSS/JS framework. It's designed to be a simple, easy to use and quick way of getting a responsive web project off the ground.",
+			"This project was originally conceived as an opportunity for me to learn SASS back in 2013 and I’ve slowly continued development on it ever since - subsequently adding technologies such as Gulp, Babel and Browserify as I’ve learned how to properly leverage them."
+		]
+	},
+	"Schooltopia" : {
+		"company": "Side Project",
+		"url": "http://www.schooltopia.co.uk",
+		"urlText" : "View Schooltopia",
+		"screenshots": [
+			{
+				"url": "img/portfolio/sideprojects/tilepuzzle/01.png",
+				"scroll": false
+			}
+		],
+		"description": [
+			"Schooltopia is a website offering web design services to primary schools. Building upon the knowledge and experience I gained at Red HQ, I wanted to create a secondary portfolio website specifically targeting schools.",
+			"This means in the future if I have the desire and the freelance capacity to build more school websites, I already have something to work with.",
+		]
+	},
+	"Tile Puzzle Game" : {
+		"company": "Side Project",
+		"url": "https://codepen.io/lukedidit/full/yOeePo/",
+		"urlText" : "Play Tile Puzzle Game",
+		"screenshots": [
+			{
+				"url": "img/portfolio/sideprojects/tilepuzzle/01.png",
+				"scroll": false
+			}
+		],
+		"description": [
+			"This was a small side project of mine to put into practice what at the time I had recently learned about object-orientated programming in JavaScript. I feel I learn best when doing, so if I learn something substantial I tend to build upon that with a practical exercise.",
+			"There’s 4 different variations to tackle. See how quickly you can solve them!"
 		]
 	},
 	"Brand Toolkit" : {
