f2d567597cacbe9c2545199ed690be9fe57accfe
Luke Harrison <luke.harrison@plus.net>
2016-05-03 17:00:23 +0100

    Added loader

--- dev/js/partials/modules/global.js
+++ dev/js/partials/modules/global.js
@@ -7,11 +7,29 @@ module.exports = (function(){
 	|--------------------------------------------------------------------
 	| GENERATE CURRENT YEAR
 	|--------------------------------------------------------------------
-	*/
-
-	
+	*/	
 
 	var year = document.querySelector(".year");	
 	year.innerText = (new Date).getFullYear();
+
+	/*
+	|--------------------------------------------------------------------
+	| DISABLE LOADER ON PAGE LOAD
+	|--------------------------------------------------------------------
+	*/	
+
+	window.onload = function(){
+
+		var body = document.querySelector("body"),
+			feature = document.querySelector(".banner--about.animated .feature-full");
+
+		// Remove loader icon
+		body.classList.remove("loading");
+
+		// If on about page start feature animation
+		if(body.classList.contains("about")){
+			feature.style.animationPlayState = "running"; 
+		}
+	};
 
 })();
