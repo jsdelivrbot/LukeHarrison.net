869bfab44a3bf2830b9aecaae7e53fc46bd5548d
Luke Harrison <luke@lukeharrison.net>
2017-11-03 21:07:27 +0000

    Added published page

--- dev/data/published.js
+++ dev/data/published.js
@@ -1,56 +1,77 @@
 module.exports = [
 	{
 		"title": "Web Designer Magazine",
-		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"description": "Web Designer is the print premier magazine for aspiring online creatives and industry professionals.",
 		"logo": "logo-webdesigner",
 		"logoSVG": true,
 		"logoClass": "c-icon c-icon--webdesigner",
 		"entries": [
 			{
-				"title": "Setup site theming with CSS variables",
-				"subtitle": "Issue 0264 | July 2017",
-				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
-				"url": "http://www.google.co.uk",
-				"urlText": "Buy!!!"
+				"title": "Managing CSS Classes with JavaScript & Data Attributes",
+				"subtitle": "Issue 267 | October 2017",
+				"description": "Create a reusable JavaScript function which uses data attributes to easily manage CSS class changes.",
+				"url": "https://www.myfavouritemagazines.co.uk/web-designer-print-back-issues/web-designer-issue-267/",
+				"urlText": "View Magazine"
 			},
 			{
-				"title": "BLA 2",
-				"subtitle": "Issue 0264 | July 2017",
-				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
-				"url": "http://www.google.co.uk",
-				"urlText": "Buy!!!"
+				"title": "Simplify Media Queries with SASS",
+				"subtitle": "Issue 266 | September 2017",
+				"description": "Media queries are a cornerstone of responsive web design. Employ the power of SASS to make them easier and quicker to use.",
+				"url": "https://www.myfavouritemagazines.co.uk/web-designer-print-back-issues/web-designer-issue-266/",
+				"urlText": "View Magazine"
+			},
+			{
+				"title": "Generate a Static Blog with Gulp & Nunjucks",
+				"subtitle": "Issue 265 | August 2017",
+				"description": "How to generate a static blog using task runner Gulp and Mozilla’s templating engine Nunjucks.",
+				"url": "https://www.myfavouritemagazines.co.uk/web-designer-print-back-issues/web-designer-issue-265/",
+				"urlText": "View Magazine"
+			},
+			{
+				"title": "Setup Site Theming with CSS variables",
+				"subtitle": "Issue 264 | July 2017",
+				"description": "Use CSS variables and Web Storage API to implement website theming which persists across sessions.",
+				"url": "https://www.myfavouritemagazines.co.uk/web-designer-print-back-issues/web-designer-issue-264/",
+				"urlText": "View Magazine"
 			}
 		]
 	},
 	{
 		"title": "CSS Tricks",
-		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"description": "CSS-Tricks is a popular website about all things web design and development.",
 		"logo": "logo-csstricks",
 		"logoSVG": true,
 		"logoClass": "c-icon c-icon--csstricks",
 		"entries": [
 			{
-				"title": "Setup site theming with CSS variables",
-				"subtitle": "Issue 0264 | July 2017",
-				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
-				"url": "http://www.google.co.uk",
-				"urlText": "Buy!!!"
+				"title": "Managing State in CSS with Reusable JavaScript Functions - Part 2",
+				"subtitle": "May 2017",
+				"description": "Continuing from part 1, I build upon and improve the class toggling reusable JavaScript function.",
+				"url": "https://css-tricks.com/managing-state-css-reusable-javascript-functions-part-2/",
+				"urlText": "Read Article"
+			},
+			{
+				"title": "Managing State in CSS with Reusable JavaScript Functions - Part 1",
+				"subtitle": "April 2017",
+				"description": "Determining the most efficient way of managing state can be a challenging issue in CSS. I explore one solution.",
+				"url": "https://css-tricks.com/managing-state-css-reusable-javascript-functions/",
+				"urlText": "Read Article"
 			}
 		]
 	},
 	{
 		"title": "Speckyboy Design Magazine",
-		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"description": "Speckyboy is an online magazine for web designers.",
 		"logo": "/img/logo-speckyboy.png",
 		"logoSVG": false,
 		"logoClass": "c-icon c-icon--speckyboy",
 		"entries": [
 			{
-				"title": "Setup site theming with CSS variables",
-				"subtitle": "Issue 0264 | July 2017",
-				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
-				"url": "http://www.google.co.uk",
-				"urlText": "Buy!!!"
+				"title": "Naming conventions for sizes in scalable CSS",
+				"subtitle": "February 2017",
+				"description": "In this article, I explore how naming conventions can be used to represent sizes in scalable CSS.",
+				"url": "https://speckyboy.com/naming-conventions-sizes-scalable-css/",
+				"urlText": "Read Article"
 			}
 		]
 	}
