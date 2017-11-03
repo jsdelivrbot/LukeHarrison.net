e67fb5949a73af9e2381e883cdef9d46988759a7
Luke Harrison <luke@lukeharrison.net>
2017-11-03 17:06:29 +0000

    WIP

---
+++ dev/data/published.js
@@ -0,0 +1,57 @@
+module.exports = [
+	{
+		"title": "Web Designer Magazine",
+		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"logo": "logo-webdesigner",
+		"logoSVG": true,
+		"logoClass": "c-icon c-icon--webdesigner",
+		"entries": [
+			{
+				"title": "Setup site theming with CSS variables",
+				"subtitle": "Issue 0264 | July 2017",
+				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
+				"url": "http://www.google.co.uk",
+				"urlText": "Buy!!!"
+			},
+			{
+				"title": "BLA 2",
+				"subtitle": "Issue 0264 | July 2017",
+				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
+				"url": "http://www.google.co.uk",
+				"urlText": "Buy!!!"
+			}
+		]
+	},
+	{
+		"title": "CSS Tricks",
+		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"logo": "logo-csstricks",
+		"logoSVG": true,
+		"logoClass": "c-icon c-icon--csstricks",
+		"entries": [
+			{
+				"title": "Setup site theming with CSS variables",
+				"subtitle": "Issue 0264 | July 2017",
+				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
+				"url": "http://www.google.co.uk",
+				"urlText": "Buy!!!"
+			}
+		]
+	},
+	{
+		"title": "Speckyboy Design Magazine",
+		"description": "Web Designer is the premier magazine for aspiring online creatives and industry professionals.",
+		"logo": "/img/logo-speckyboy.png",
+		"logoSVG": false,
+		"logoClass": "c-icon c-icon--speckyboy",
+		"entries": [
+			{
+				"title": "Setup site theming with CSS variables",
+				"subtitle": "Issue 0264 | July 2017",
+				"description": "Combine CSS variables and the Web Storage API to change the layout and colours, and store your themes for future use.",
+				"url": "http://www.google.co.uk",
+				"urlText": "Buy!!!"
+			}
+		]
+	}
+]
